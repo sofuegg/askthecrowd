c8fbe42d4d22a13183b6269ec3ce400b655083e4
John Fu <42628628+sofuegg@users.noreply.github.com>
2019-08-21 13:19:16 +0800

    Update asking-page.js

--- pages/asking-page/asking-page.js
+++ pages/asking-page/asking-page.js
@@ -1,8 +1,8 @@
 // pages/asking-page/asking-page.js
 const app = getApp()
 const AV = require('../../utils/av-weapp-min.js');
-const baseUrl = "http://localhost:3000/api/v1/"
-// const baseUrl = "https://ask-the-crowd.wogengapp.cn/api/v1/"
+// const baseUrl = "http://localhost:3000/api/v1/"
+const baseUrl = "https://ask-the-crowd.wogengapp.cn/api/v1/"
 
 Page({
 
