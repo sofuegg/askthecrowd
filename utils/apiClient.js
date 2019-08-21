a6748b055b33a12499f0501cb88b0e7acb8411c2
John Fu <uctpchf@ymail.com>
2019-08-21 15:48:26 +0800

    textlength

--- utils/apiClient.js
+++ utils/apiClient.js
@@ -1,11 +1,11 @@
-// const baseUrl = "https://ask-the-crowd.wogengapp.cn/api/v1/"
-const baseUrl = 'http://localhost:3000/api/v1/'
+const baseUrl = "https://ask-the-crowd.wogengapp.cn/api/v1/"
+// const baseUrl = 'http://localhost:3000/api/v1/'
 
 const getQuestions = (options) => {
   const { success, fail } = options
 
   return wx.request({
-    url: baseUrl + "questions",
+    url: baseUrl + "questions/",
     method: "get",
     success,
     fail
@@ -16,7 +16,7 @@ const getMyresponses = (options) => {
   const { userid, success, fail } = options
   const user_id = wx.getStorageSync('userid')
   return wx.request({
-    url: baseUrl + `questions/my_responses/${user_id}`,
+    url: baseUrl + `questions/my_responses/${user_id}/`,
     method: "get",
     success,
     fail
@@ -27,7 +27,7 @@ const getAskedquestions = (options) => {
   const { userid, success, fail } = options
   const user_id = wx.getStorageSync('userid')
   return wx.request({
-    url: baseUrl + `questions/asked_questions/${user_id}`,
+    url: baseUrl + `questions/asked_questions/${user_id}/`,
     method: "get",
     success,
     fail
@@ -39,7 +39,7 @@ const getQuestion = options => {
   const { id, success, fail } = options
 
   return wx.request({
-    url: baseUrl + `questions/${id}`,
+    url: baseUrl + `questions/${id}/`,
     method: "get",
     success,
     fail
@@ -50,7 +50,7 @@ const activateQuestion = options => {
   const { id, success, fail } = options
 
   return wx.request({
-    url: baseUrl + `questions/${id}`,
+    url: baseUrl + `questions/${id}/`,
     method: "put",
     success,
     fail
@@ -62,7 +62,7 @@ const createAnswer = options => {
   const { data, success, fail } = options
 
   return wx.request({
-    url: baseUrl + "answers",
+    url: baseUrl + "answers/",
     method: "post",
     data,
     success,
@@ -74,7 +74,7 @@ const shareQuestion = options => {
   const { id, success, fail } = options
 
   return wx.request({
-    url: baseUrl + `answers/${id}`,
+    url: baseUrl + `answers/${id}/`,
     method: "get",
     success,
     fail
